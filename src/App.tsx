ca247d9a7f62f439bf6ffbd3360912c58efbf696
borhanrahi <borhanrahi111@gmail.com>
2023-05-21 22:40:39 +0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
 // import ListGroup from "./components/ListGroup";
 // import { useState } from "react";
 
+
 // function App() {
 //   const items = [
 //     "New York",
@@ -41,16 +42,65 @@
 
 // export default App;
 
+// import Like from './components/Like'
 
-import Like from './components/Like'
+// function App() {
+//   return (
+//     <div>
+//       <Like onClick={()=> console.log('Clicked!')
+//       }/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import { useState } from "react";
+
+// function App() {
+//   const [game, setGame] = useState({
+//     id: 1,
+//     player: {
+//       name: "John",
+//     },
+//   });
+
+//   const handleClick = () => {
+//     setGame({
+//       ...game,
+//       player: {
+//         ...game.player,
+//         name: "Bob",
+//       },
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={handleClick}>Change Player Name</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useState } from 'react'
+
 
 function App() {
+  const [tags, setTags] = useState(['tag1', 'tag2']); 
+  const handleClick = () => {
+    setTags([...tags, 'tag3']);
+    setTags(tags.filter((tag) => tag !== 'tag3'));
+    setTags(tag => tag.filter((tag) => tag !== 'tag3'));
+  }
+
   return (
     <div>
-      <Like onClick={()=> console.log('Clicked!')
-      }/> 
+      <button onClick={handleClick}>Add Tag</button>
     </div>
-  )
+  );
 }
 
 export default App
+
